b5d1ee56ee8e005c65c0967eec429bc307af770a
gd180 <gdoukas@epu.ntua.gr>
2022-07-13 20:19:33 +0300

    Revert "Merge remote-tracking branch 'origin/samseg_results' into dev"
    This reverts commit 7acbe5e9b79535b7b2915ee235decf5e07630d49, reversing
    changes made to 3f48b2d6b35a5f6e73ee9ec7797f67da8af33afb.

---
+++ src/components/base-components/PageError.jsx
@@ -0,0 +1,19 @@
+import React, {useContext, useEffect} from 'react';
+// import {useHistory} from "react-router-dom";
+// import {AuthContext} from "../contexts/AuthContext";
+//
+// import {servicesInfo} from "../servicesInfo";
+// import InitialPageService from "./InitialPageService";
+
+
+function PageError() {
+    return (
+        <React.Fragment>
+            {/*<h1>{document.title}</h1>*/}
+            <h1>Error</h1>
+            <h3>Something went wrong. Please try again later</h3>
+        </React.Fragment>
+    );
+}
+
+export default PageError;
