c823a983c60c1138bd1b2075fc18a3a52867cc01
gd180 <gdoukas@epu.ntua.gr>
2022-12-09 19:58:40 +0200

    Homoscedasticity.jsx Left panel Functionality updated.

--- src/pages/hypothesis_testing/Homoscedasticity.jsx
+++ src/pages/hypothesis_testing/Homoscedasticity.jsx
@@ -10,9 +10,10 @@ import {
     ListItem,
     ListItemText,
     MenuItem,
-    Select, TextField,
+    Select, TextareaAutosize, TextField,
     Typography
 } from "@mui/material";
+import qs from "qs";
 
 class Homoscedasticity extends React.Component {
     constructor(props){
@@ -23,19 +24,24 @@ class Homoscedasticity extends React.Component {
             test_data: [],
             //Values selected currently on the form
             selected_column: "",
-            selected_column2: "",
             selected_method: "Bartlett",
-            selected_center: "median"
-
+            selected_center: "median",
+            selected_independent_variables: [],
+            center_show:false
         };
         //Binding functions of the class
         this.fetchColumnNames = this.fetchColumnNames.bind(this);
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleSelectColumnChange = this.handleSelectColumnChange.bind(this);
-        this.handleSelectColumn2Change = this.handleSelectColumn2Change.bind(this);
         this.handleSelectMethodChange = this.handleSelectMethodChange.bind(this);
         this.handleSelectCenterChange = this.handleSelectCenterChange.bind(this);
+
+        this.onClickButton = this.onClickButton.bind(this)
+        this.handleSelectIndependentVariableChange = this.handleSelectIndependentVariableChange.bind(this);
+        this.clear = this.clear.bind(this);
+        this.selectAll = this.selectAll.bind(this);
+
         // // Initialise component
         // // - values of channels from the backend
         this.fetchColumnNames();
@@ -46,7 +52,12 @@ class Homoscedasticity extends React.Component {
      * Call backend endpoint to get column names
      */
     async fetchColumnNames(url, config) {
-        API.get("return_columns", {}).then(res => {
+        const params = new URLSearchParams(window.location.search);
+        API.get("return_columns",
+                {params: {
+                    run_id: params.get("run_id"),
+                    step_id: params.get("step_id")
+                }}).then(res => {
             this.setState({column_names: res.data.columns})
         });
     }
@@ -56,12 +67,20 @@ class Homoscedasticity extends React.Component {
      */
     async handleSubmit(event) {
         event.preventDefault();
+        const params = new URLSearchParams(window.location.search);
 
         // Send the request
         API.get("check_homoscedasticity",
                 {
-                    params: {column_1: this.state.selected_column, column_2:this.state.selected_column2, name_test: this.state.selected_method, center: this.state.selected_center}
-                }
+                    params: {
+                        run_id: params.get("run_id"),
+                        step_id: params.get("step_id"),
+                        columns: this.state.selected_independent_variables,
+                        name_of_test: this.state.selected_method,
+                        center: this.state.selected_center},
+                    paramsSerializer : params => {
+                        return qs.stringify(params, { arrayFormat: "repeat" })
+                }}
         ).then(res => {
             this.setState({test_data: res.data})
         });
@@ -71,47 +90,77 @@ class Homoscedasticity extends React.Component {
     /**
      * Update state when selection changes in the form
      */
+    onClickButton(event) {
+        const cnt = this.state.selected_independent_variables.map((item) => ({item}.length))
+        if (cnt<2)
+        {
+            alert('At least TWO variables must be selected!')
+            event.preventDefault();
+        }
+    }
+
     handleSelectColumnChange(event){
         this.setState( {selected_column: event.target.value})
     }
-    handleSelectColumn2Change(event){
-        this.setState( {selected_column2: event.target.value})
-    }
     handleSelectMethodChange(event){
         this.setState( {selected_method: event.target.value})
+        if (event.target.value=="Bartlett"){
+            this.setState({center_show:false})
+        }
+        else {
+            this.setState({center_show:true});
+        }
     }
     handleSelectCenterChange(event){
         this.setState( {selected_center: event.target.value})
     }
 
+    handleSelectIndependentVariableChange(event){
+        this.setState( {selected_independent_variables: event.target.value})
+    }
+
+    clear(){
+        this.setState({selected_independent_variables: []})
+    }
+    selectAll(){
+        this.setState({selected_independent_variables: this.state.column_names})
+    }
+
     render() {
         return (
                 <Grid container direction="row">
-                    <Grid item xs={2}  sx={{ borderRight: "1px solid grey"}}>
-                        <Typography variant="h5" sx={{ flexGrow: 1, textAlign: "center" }} noWrap>
-                            Data Preview
-                        </Typography>
-                        <hr/>
-                        <List>
-                            {this.state.column_names.map((column) => (
-                                    <ListItem> <ListItemText primary={column}/></ListItem>
-                            ))}
-                        </List>
-                    </Grid>
-                    <Grid item xs={5} sx={{ borderRight: "1px solid grey"}}>
+                    <Grid item xs={3} sx={{ borderRight: "1px solid grey"}}>
                         <Typography variant="h5" sx={{ flexGrow: 1, textAlign: "center" }} noWrap>
                             Select method for Homoscedasticity check
                         </Typography>
                         <hr/>
+                        <FormControl sx={{m: 1, width:'90%'}} size={"small"} >
+                            <FormHelperText>Selected Variables</FormHelperText>
+                            <List style={{fontSize:'9px', backgroundColor:"lightgreen"}}>
+                                {this.state.selected_independent_variables.map((column) => (
+                                        <ListItem disablePadding
+                                                  >
+                                            <ListItemText
+                                                    primaryTypographyProps={{fontSize: '10px'}}
+                                                    primary={'•  ' + column}
+                                            />
+
+                                        </ListItem>
+                                        ))}
+                            </List>
+                        </FormControl>
+                        <hr/>
+
                         <form onSubmit={this.handleSubmit}>
-                            <FormControl sx={{m: 1, minWidth: 120}}>
+                            <FormControl sx={{m: 1, width:'90%'}} size={"small"}>
                                 <InputLabel id="column-selector-label">Column</InputLabel>
                                 <Select
                                         labelId="column-selector-label"
                                         id="column-selector"
-                                        value= {this.state.selected_column}
+                                        value= {this.state.selected_independent_variables}
+                                        multiple
                                         label="Column"
-                                        onChange={this.handleSelectColumnChange}
+                                        onChange={this.handleSelectIndependentVariableChange}
                                 >
                                     <MenuItem value="">
                                         <em>None</em>
@@ -120,27 +169,15 @@ class Homoscedasticity extends React.Component {
                                             <MenuItem value={column}>{column}</MenuItem>
                                     ))}
                                 </Select>
-                                <FormHelperText>Select Column 01 for Homoscedasticity check</FormHelperText>
+                                <FormHelperText>Select Variables</FormHelperText>
+                                <Button size={"small"} onClick={this.selectAll}>
+                                    Select All Variables
+                                </Button>
+                                <Button size={"small"} onClick={this.clear}>
+                                    Clear Selections
+                                </Button>
                             </FormControl>
-                            <FormControl sx={{m: 1, minWidth: 120}}>
-                                <InputLabel id="column2-selector-label">Column</InputLabel>
-                                <Select
-                                        labelId="column2-selector-label"
-                                        id="column2-selector"
-                                        value= {this.state.selected_column2}
-                                        label="Column"
-                                        onChange={this.handleSelectColumn2Change}
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    {this.state.column_names.map((column) => (
-                                            <MenuItem value={column}>{column}</MenuItem>
-                                    ))}
-                                </Select>
-                                <FormHelperText>Select Column 02 for Homoscedasticity check</FormHelperText>
-                            </FormControl>
-                            <FormControl sx={{m: 1, minWidth: 120}}>
+                            <FormControl sx={{m: 1, width:'90%'}} size={"small"}>
                                 <InputLabel id="method-selector-label">Method</InputLabel>
                                 <Select
                                         labelId="method-selector-label"
@@ -156,8 +193,9 @@ class Homoscedasticity extends React.Component {
                                 </Select>
                                 <FormHelperText>Specify which method to use.</FormHelperText>
                             </FormControl>
-                            <FormControl sx={{m: 1, minWidth: 120}}>
-                                <Select
+                            <FormControl style={{ display: (this.state.center_show ? 'block' : 'none') }}>
+                                <InputLabel id="center-selector-label">Function</InputLabel>
+                                <Select sx={{m: 1, width:'90%'}} size={"small"}
                                         labelid="center-selector-label"
                                         id="center-selector"
                                         value= {this.state.selected_center}
@@ -170,12 +208,12 @@ class Homoscedasticity extends React.Component {
                                 </Select>
                                 <FormHelperText>Keyword argument controlling which function of the data is used in computing the test statistic. The default is ‘median’.</FormHelperText>
                             </FormControl>
-                            <Button variant="contained" color="primary" type="submit">
+                            <Button variant="contained" color="primary" type="submit" onClick={this.onClickButton}>
                                 Submit
                             </Button>
                         </form>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={9}>
                         <Typography variant="h5" sx={{ flexGrow: 1, textAlign: "center" }} noWrap>
                             Result Visualisation
                         </Typography>
