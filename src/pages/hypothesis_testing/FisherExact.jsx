904aa378d2a5dd9ae186ab31fc852a6b019727b3
gd180 <gdoukas@epu.ntua.gr>
2023-03-03 15:02:40 +0200

    added page FisherExact.jsx (input: 2 binary columns from a dataset)

--- src/pages/hypothesis_testing/FisherExact.jsx
+++ src/pages/hypothesis_testing/FisherExact.jsx
@@ -4,34 +4,54 @@ import {
     Button,
     FormControl,
     FormHelperText,
-    Grid, InputLabel, MenuItem, Select,
+    Grid, InputLabel, MenuItem, Select, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
     TextField,
     Typography
 } from "@mui/material";
+import JsonTable from "ts-react-json-table";
+import Paper from "@mui/material/Paper";
 
 class FisherExact extends React.Component {
     constructor(props){
         super(props);
         const params = new URLSearchParams(window.location.search);
         this.state = {
-            // List of columns in dataset
             test_data: {
-                odd_ratio:"",
-                p_value:"",
+                odd_ratio: "",
+                p_value: "",
+                crosstab: ""
             },
-            selected_top_left: "",
-            selected_top_right: "",
-            selected_bottom_left: "",
-            selected_bottom_right: "",
+            binary_columns: [],
+            crosstab_cols:[],
+            crosstab_index:[],
+            crosstab_data:[],
+            crosstab_data_0:[],
+            crosstab_data_1:[],
+            crosstab_data_2:[],
+            result_crosstab:"",
+            selected_column_variable: "",
+            selected_row_variable: "",
             selected_alternative: "two-sided"
         };
         //Binding functions of the class
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleSelectTopLeftChange = this.handleSelectTopLeftChange.bind(this);
-        this.handleSelectTopRightChange = this.handleSelectTopRightChange.bind(this);
-        this.handleSelectBottomLeftChange = this.handleSelectBottomLeftChange.bind(this);
-        this.handleSelectBottomRightChange = this.handleSelectBottomRightChange.bind(this);
+        this.fetchBinaryColumnNames = this.fetchBinaryColumnNames.bind(this);
+        this.handleSelectColumnVariableChange = this.handleSelectColumnVariableChange.bind(this);
+        this.handleSelectRowVariableChange = this.handleSelectRowVariableChange.bind(this);
         this.handleSelectAlternativeChange = this.handleSelectAlternativeChange.bind(this);
+        this.fetchBinaryColumnNames();
+    }
+
+    async fetchBinaryColumnNames(url, config) {
+        const params = new URLSearchParams(window.location.search);
+        API.get("return_binary_columns",
+                {params: {
+                        workflow_id: params.get("workflow_id"), run_id: params.get("run_id"),
+                        step_id: params.get("step_id")
+                    }}).then(res => {
+            // console.log(res.data.columns)
+            this.setState({binary_columns: res.data.columns})
+        });
     }
 
     async handleSubmit(event) {
@@ -45,32 +65,43 @@ class FisherExact extends React.Component {
                         workflow_id: params.get("workflow_id"),
                         run_id: params.get("run_id"),
                         step_id: params.get("step_id"),
-                        variable_top_left: this.state.selected_top_left,
-                        variable_top_right: this.state.selected_top_right,
-                        variable_bottom_left: this.state.selected_bottom_left,
-                        variable_bottom_right: this.state.selected_bottom_right,
+                        variable_column: this.state.selected_column_variable,
+                        variable_row: this.state.selected_row_variable,
                         alternative: this.state.selected_alternative
                         }
                 }
         ).then(res => {
             this.setState({test_data: res.data})
+            const resultJson = JSON.parse(res.data.crosstab);
+            this.setState({crosstab_cols:resultJson['columns']})
+            this.setState({crosstab_index:resultJson['index']})
+            this.setState({crosstab_data:resultJson['data']})
+            this.setState({crosstab_data_0:resultJson['data'][0]})
+            this.setState({crosstab_data_1:resultJson['data'][1]})
+            this.setState({crosstab_data_2:resultJson['data'][2]})
         });
     }
 
     /**
      * Update state when selection changes in the form
      */
-    handleSelectTopLeftChange(event){
-        this.setState( {selected_top_left: event.target.value})
+    handleSelectRowVariableChange(event){
+        if (event.target.value != this.state.selected_column_variable)
+        {
+            this.setState( {selected_row_variable: event.target.value})
+        }else{
+            alert("You must select a different variable.")
+            return
+        }
     }
-    handleSelectTopRightChange(event){
-        this.setState( {selected_top_right: event.target.value})
-    }
-    handleSelectBottomLeftChange(event){
-        this.setState( {selected_bottom_left: event.target.value})
-    }
-    handleSelectBottomRightChange(event){
-        this.setState( {selected_bottom_right: event.target.value})
+    handleSelectColumnVariableChange(event){
+        if (event.target.value != this.state.selected_row_variable)
+        {
+            this.setState( {selected_column_variable: event.target.value})
+        }else{
+            alert("You must select a different variable.")
+            return
+        }
     }
     handleSelectAlternativeChange(event){
         this.setState( {selected_alternative: event.target.value})
@@ -85,46 +116,41 @@ class FisherExact extends React.Component {
                         </Typography>
                         <hr/>
                         <form onSubmit={this.handleSubmit}>
-                            <FormControl sx={{m: 1, width:'90%'}} >
-                                <TextField size={"small"}
-                                           labelid="top-left-label"
-                                           id="top-left-selector"
-                                           value= {this.state.selected_top_left}
-                                           label="In sample with A"
-                                           onChange={this.handleSelectTopLeftChange}
-                                />
-                                <FormHelperText>The number of “cases” (i.e. occurrence of disease or other event of interest)
-                                    among the sample of “exposed” individuals.</FormHelperText>
+                            <FormControl sx={{m: 1, minWidth: 120}}>
+                                <InputLabel id="column-selector-label">Variable</InputLabel>
+                                <Select
+                                        labelId="column-selector-label"
+                                        id="column-selector"
+                                        value= {this.state.selected_row_variable}
+                                        label="Column"
+                                        onChange={this.handleSelectRowVariableChange}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    {this.state.binary_columns.map((column) => (
+                                            <MenuItem value={column}>{column}</MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText>Select Row Variable</FormHelperText>
                             </FormControl>
-                            <FormControl sx={{m: 1, width:'90%'}} >
-                                <TextField size={"small"}
-                                           labelid="top-right-label"
-                                           id="top-right-selector"
-                                           value= {this.state.selected_top_right}
-                                           label="In sample with Not-A"
-                                           onChange={this.handleSelectTopRightChange}
-                                />
-                                <FormHelperText>Count of unexposed individuals with outcome.</FormHelperText>
-                            </FormControl>
-                            <FormControl sx={{m: 1, width:'90%'}} >
-                                <TextField size={"small"}
-                                           labelid="bottom-left-label"
-                                           id="bottom-left-selector"
-                                           value= {this.state.selected_bottom_left}
-                                           label="Not in sample with A"
-                                           onChange={this.handleSelectBottomLeftChange}
-                                />
-                                <FormHelperText>Count of exposed individuals without outcome.</FormHelperText>
-                            </FormControl>
-                            <FormControl sx={{m: 1, width:'90%'}} >
-                                <TextField size={"small"}
-                                           labelid="bottom-right-label"
-                                           id="bottom-right-selector"
-                                           value= {this.state.selected_bottom_right}
-                                           label="Not in sample with Not-A"
-                                           onChange={this.handleSelectBottomRightChange}
-                                />
-                                <FormHelperText>Count of unexposed individuals without outcome.</FormHelperText>
+                            <FormControl sx={{m: 1, minWidth: 120}}>
+                                <InputLabel id="column-selector-label">Variable</InputLabel>
+                                <Select
+                                        labelId="column-selector-label"
+                                        id="column-selector"
+                                        value= {this.state.selected_column_variable}
+                                        label="Column"
+                                        onChange={this.handleSelectColumnVariableChange}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    {this.state.binary_columns.map((column) => (
+                                            <MenuItem value={column}>{column}</MenuItem>
+                                    ))}
+                                </Select>
+                                <FormHelperText>Select Column variable</FormHelperText>
                             </FormControl>
                             <FormControl sx={{m: 1, width:'90%'}} size={"small"}>
                                 <InputLabel id="alternative-selector-label">Alternative</InputLabel>
@@ -152,10 +178,63 @@ class FisherExact extends React.Component {
                         </Typography>
                         <hr/>
                         <div>
-                            <Typography variant="h6" color='royalblue' sx={{ flexGrow: 1, textAlign: "Left", padding:'20px'}} >
-                                odd_ratio = { this.state.test_data.odd_ratio}</Typography>
-                            <Typography variant="h6" color='royalblue' sx={{ flexGrow: 1, textAlign: "Left", padding:'20px'}} >
-                                p_value = {this.state.test_data.p_value}</Typography>
+                            <Typography variant="h6" color='royalblue' sx={{ flexGrow: 1, textAlign: "center", padding:'20px'}} >
+                                { this.state.selected_row_variable} * {this.state.selected_column_variable} Crosstabulation</Typography>
+                            <TableContainer component={Paper} className="ExtremeValues" sx={{width:'50%', minWidth:'120px'}}>
+                                <Table sx={{textAlign:"right"}}>
+                                    <TableHead>
+                                        <TableRow >
+                                            <TableCell sx={{border:'none'}}></TableCell>
+                                            <TableCell colSpan={3} className="tableHeadCell" style={{fontWeight:'bold', borderTop:'none'}}>{this.state.selected_column_variable}</TableCell></TableRow>
+                                        <TableRow>
+                                            <TableCell align="center" style={{fontWeight:'bold' , borderTop:'none'}} className="tableHeadCell">{this.state.selected_row_variable}</TableCell>
+                                            {this.state.crosstab_cols.map((column) => (
+                                                    <TableCell className="tableHeadCell" value={column}>{column}</TableCell>
+                                            ))}
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell sx={{fontWeight:'bold', width:'20%'}} className="tableCell" >{this.state.crosstab_index[0]}</TableCell>
+                                            {this.state.crosstab_data_0.map((column) => (
+                                                    <TableCell className="tableCell" value={column}>{column}</TableCell>
+                                            ))}
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{fontWeight:'bold'}} className="tableCell" >{this.state.crosstab_index[1]}</TableCell>
+                                            {this.state.crosstab_data_1.map((column) => (
+                                                    <TableCell className="tableCell" value={column}>{column}</TableCell>
+                                            ))}
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell sx={{fontWeight:'bold'}} className="tableCell" >{this.state.crosstab_index[2]}</TableCell>
+                                            {this.state.crosstab_data_2.map((column) => (
+                                                    <TableCell className="tableCell" value={column}>{column}</TableCell>
+                                            ))}
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <br/>
+                            <br/>
+                            <TableContainer component={Paper} className="ExtremeValues" sx={{width:'70%', minWidth:'120px'}}>
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell/>
+                                            <TableCell align="center" style={{fontWeight:'bold' , borderTop:'none'}}>odd_ratio</TableCell>
+                                            <TableCell align="center" style={{fontWeight:'bold' , borderTop:'none'}}>p_value</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        <TableRow>
+                                            <TableCell align="center" style={{fontWeight:'bold' , borderTop:'none'}}>Fisher's Exact test</TableCell>
+                                            <TableCell align="center">{ Number.parseFloat(this.state.test_data.odd_ratio).toExponential(4)}</TableCell>
+                                            <TableCell align="center">{Number.parseFloat(this.state.test_data.p_value).toExponential(4)}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
                         </div>
                     </Grid>
                 </Grid>
