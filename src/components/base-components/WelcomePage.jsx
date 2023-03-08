b0c83d6b58167595371f32758df31f4be4ffea8e
mikailo <m.kontoulis.cs@gmail.com>
2023-03-08 19:07:52 +0200

    -Updated stepid of freesurfer function -Finished freesurfer page funcitonality (Pending correct redirect to result page if needed, dependent on finished result page)

--- src/components/base-components/WelcomePage.jsx
+++ src/components/base-components/WelcomePage.jsx
@@ -336,7 +336,7 @@ function WelcomePage() {
                                 variant="contained"
                                 size="medium"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%",borderLeft : "1px solid black", position: "absolute"}} />}
-                                onClick={redirectToPage.bind(this,1, 1, 1, "free_surfer", ["saved"], ["psg1 anonym2.edf"])}
+                                onClick={redirectToPage.bind(this,1, 1, 3, "free_surfer", ["saved"], ["psg1 anonym2.edf"])}
                         >
                             Free Surfer
                         </Button>
@@ -345,7 +345,7 @@ function WelcomePage() {
                                 // variant="outlined"
                                 size="medium"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%",borderLeft : "1px solid black", position: "absolute"}} />}
-                                onClick={redirectToPage.bind(this,1, 1, 1, "recon_all_results", ["saved"], ["psg1 anonym2.edf"])}
+                                onClick={redirectToPage.bind(this,1, 1, 3, "recon_all_results", ["saved"], ["psg1 anonym2.edf"])}
                         >
                             Recon-All Results
                         </Button>
@@ -354,7 +354,7 @@ function WelcomePage() {
                                 variant="contained"
                                 size="medium"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%",borderLeft : "1px solid black", position: "absolute"}} />}
-                                onClick={redirectToPage.bind(this,1, 1, 1, "samseg_results", ["saved"], ["psg1 anonym2.edf"])}
+                                onClick={redirectToPage.bind(this,1, 1, 3, "samseg_results", ["saved"], ["psg1 anonym2.edf"])}
                         >
                             Samseg Results
                         </Button>
