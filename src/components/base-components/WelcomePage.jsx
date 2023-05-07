9dcba79425c04cab29c82c94e8446d9661da4b05
gd180 <gdoukas@epu.ntua.gr>
2023-05-07 21:44:49 +0300

    KMeansFunctionPage.jsx
    fixed:
    updated with file selector + selected variables + proceed

--- src/components/base-components/WelcomePage.jsx
+++ src/components/base-components/WelcomePage.jsx
@@ -836,7 +836,7 @@ function WelcomePage() {
                                 fullWidth
                                 variant="contained"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%",borderLeft : "1px solid black", position: "absolute"}} />}
-                                onClick={redirectToPage.bind(this,1,1, 2, "KMeans", ["saved"], ["demo_sample_questionnaire.csv"])}
+                                onClick={redirectToPage.bind(this,"3fa85f64-5717-4562-b3fc-2c963f66afa6", "3fa85f64-5717-4562-b3fc-2c963f66afa6", "3fa85f64-5717-4562-b3fc-2c963f66afa6", "KMeans", ["demo"], ["expertsystem/workflow/3fa85f64-5717-4562-b3fc-2c963f66afa6/3fa85f64-5717-4562-b3fc-2c963f66afa6/3fa85f64-5717-4562-b3fc-2c963f66afa6/mescobrad_dataset.csv"])}
 
                         >
                             KMeans
