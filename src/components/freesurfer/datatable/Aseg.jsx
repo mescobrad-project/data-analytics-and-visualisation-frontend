033b48be01ac78418afe4ee334182c1f67922432
theopnt <theopountridis@gmail.com>
2023-05-17 14:26:36 +0300

    Change ip to work with docker

--- src/components/freesurfer/datatable/Aseg.jsx
+++ src/components/freesurfer/datatable/Aseg.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 import API from "../../../axiosInstance";
 import "./aseg.scss";
 
-
 class Aseg extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {aseg_path : 'http://localhost:8000/static/aseg.png',
+        let ip = "http://127.0.0.1:8000/";
+        if (process.env.REACT_APP_BASEURL)
+        {
+            ip = process.env.REACT_APP_BASEURL
+        }
+        this.state = {aseg_path : ip + 'static/aseg.png',
         };
         this.fetchData = this.fetchData.bind(this);
         this.fetchData();
