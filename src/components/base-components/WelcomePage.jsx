5a4b1427d8d50c081a1264d562c29230853d1320
mikailo <m.kontoulis.cs@gmail.com>
2023-07-12 19:47:54 +0300

    -Added tutorial visual images and formatted how they are displayed -Changed example of manual sleep staging to download correct files -Added viewer of available hypnograms -Added misc calls to backend

--- src/components/base-components/WelcomePage.jsx
+++ src/components/base-components/WelcomePage.jsx
@@ -309,7 +309,7 @@ function WelcomePage() {
                                 variant="contained"
                                 size="medium"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%", borderLeft : "1px solid black", position: "absolute"}} />}
-                                onClick={redirectToPage.bind(this,1, 1, 7, "manual_sleep_stage_classification", ["saved", "saved", "saved", "saved"], ["psg1 anonym2.edf", "new_hypnogram.csv", "new_hypnogram1.csv", "new_hypnogram2.csv"])}
+                                onClick={redirectToPage.bind(this,1, 1, 7, "manual_sleep_stage_classification", ["saved", "saved", "saved", "saved"], ["psg1 anonym2.edf", "auto_hypno_annotations.txt", "auto_hypno_annotations1.txt", "auto_hypno_annotations2.txt"])}
                         >
                             Manual Sleep Stage Classification
                         </Button>
