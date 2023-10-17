96cfaebf99e759049d4c186a05a4dbf4bec68069
mikailo <m.kontoulis.cs@gmail.com>
2023-10-18 01:29:53 +0300

    -Adding proper funcitonality in group function (WIP) - Created page and adding first calling function for data and execution

---
+++ src/components/base-components/GroupSleepSensitivityAnalysisPage.jsx
@@ -0,0 +1,224 @@
+import React from 'react';
+import API from "../../axiosInstance";
+import PropTypes from 'prop-types';
+import "../../pages/hypothesis_testing/normality_tests.scss";
+import {
+    Button, Divider,
+    FormControl,
+    FormHelperText,
+    Grid,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemText,
+    MenuItem, Modal,
+    Select, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tabs, TextField, Typography
+} from "@mui/material";
+
+// Amcharts
+// import * as am5 from "@amcharts/amcharts5";
+// import * as am5xy from "@amcharts/amcharts5/xy";
+// import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import PointChartCustom from "../ui-components/PointChartCustom";
+import RangeAreaChartCustom from "../ui-components/RangeAreaChartCustom";
+import EEGSelector from "./EEGSelector";
+import {Box} from "@mui/system";
+import ChannelSignalPeaksChartCustom from "../ui-components/ChannelSignalPeaksChartCustom";
+import EEGSelectModal from "../ui-components/EEGSelectModal";
+import {useLocation} from "react-router-dom";
+import {DataGrid, GridCell, GridToolbarContainer, GridToolbarExport} from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import InnerHTML from "dangerously-set-html-content";
+import ProceedButton from "../ui-components/ProceedButton";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "95%",
+    height: "95%",
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+function CustomToolbar() {
+    return (
+            <GridToolbarContainer>
+                <GridToolbarExport />
+            </GridToolbarContainer>
+    );
+}
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+            <div
+                    role="tabpanel"
+                    hidden={value !== index}
+                    id={`simple-tabpanel-${index}`}
+                    aria-labelledby={`simple-tab-${index}`}
+                    {...other}
+            >
+                {value === index && (
+                        <Box sx={{ p: 3 }}>
+                            <Typography>{children}</Typography>
+                        </Box>
+                )}
+            </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+class GroupSleepSensitivityAnalysisPage extends React.Component {
+    constructor(props) {
+        super(props);
+        const params = new URLSearchParams(window.location.search);
+        let ip = "http://127.0.0.1:8000/"
+        if (process.env.REACT_APP_BASEURL)
+        {
+            ip = process.env.REACT_APP_BASEURL
+        }
+        this.state = {
+            // Utils
+            channels: [],
+            tabvalue: 0,
+
+        };
+
+        //Binding functions of the class
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.fetchChannels = this.fetchChannels.bind(this);
+        this.handleTabChange = this.handleTabChange.bind(this);
+
+        this.fetchChannels()
+    }
+
+    /**
+     * Process and send the request for auto correlation and handle the response
+     */
+    async handleSubmit(event) {
+        event.preventDefault();
+        const params = new URLSearchParams(window.location.search);
+        API.get("/group_sleep_analysis_sensitivity_add_subject_add_channels_final", {
+            params: {
+                workflow_id: params.get("workflow_id"),
+                run_id: params.get("run_id"),
+                step_id: params.get("step_id"),
+                //TODO UPDATE THIS VARIABLE TO A PROPER ONE
+                sampling_frequency: 5,
+                channels_selection: null,
+                // name: this.state.selected_channel,
+                // sampling_frequency: 1/30,
+            }
+        }).then(res => {
+            console.log("Results")
+            console.log(res.data)
+        });
+    }
+
+
+    async fetchChannels() {
+        const params = new URLSearchParams(window.location.search);
+        API.get("/list/channels/group", {
+            params: {
+                workflow_id: params.get("workflow_id"),
+                run_id: params.get("run_id"),
+                step_id: params.get("step_id"),
+            }
+
+        }).then(res => {
+            this.setState({channels: res.data.channels})
+            console.log(res.data.channels)
+        });
+    }
+
+    handleTabChange(event, newvalue){
+        this.setState({tabvalue: newvalue})
+    }
+
+    debug = () => {
+        console.log("DEBUG")
+        // console.log(this.state.result_sleep_transition_matrix["figure"]["figure"])
+        // console.log(JSON.parse(this.state.result_sleep_statistic_hypnogram["sleep_statistics"]))
+        // console.log(JSON.parse(this.state.result_sleep_transition_matrix["counts_transition_matrix"]))
+        // console.log(JSON.parse(this.state.result_sleep_transition_matrix["conditional_probability_transition_matrix"]))
+        // console.log(JSON.parse(this.state.result_sleep_stability_extraction["sleep_stage_stability"]))
+        console.log(JSON.parse(this.state.result_band_power["bandpower"]))
+        // console.log(JSON.parse(this.state.result_spectogram["figure"]["figure"]))
+        console.log(this.state.result_spectogram["figure"]["figure"])
+
+    };
+
+    render() {
+        return (
+                <Grid container direction="row">
+                    <Grid item xs={4} sx={{borderRight: "1px solid grey"}}>
+                        <Typography variant="h5" sx={{flexGrow: 1, textAlign: "center"}} noWrap>
+                            Group Sleep Analysis Parameterisation
+                        </Typography>
+                        <Divider sx={{bgcolor: "black"}}/>
+
+
+                        <Divider/>
+                        {/* The form only appears when this.state.channels has any value which happens only when the forms
+                    knows what file to access*/}
+                        <form onSubmit={this.handleSubmit}>
+                            <Divider/>
+                            <Button sx={{float: "left", marginLeft: "2px"}} variant="contained" color="primary"
+                                    type="submit">
+                                Submit
+                            </Button>
+                            <ProceedButton></ProceedButton>
+                        </form>
+                        <form onSubmit={async (event) => {
+                            event.preventDefault();
+                            window.location.replace("/")
+                            // Send the request
+                        }}>
+                            <Button sx={{float: "right", marginRight: "2px"}} variant="contained" color="primary"
+                                    type="submit">
+                                Proceed >
+                            </Button>
+                        </form>
+                    </Grid>
+
+                    <Grid item xs={8}  direction='column'>
+                        <Typography variant="h5" sx={{flexGrow: 1, textAlign: "center"}} noWrap>
+                            Result Visualisation
+                        </Typography>
+                        <Divider sx={{bgcolor: "black"}}/>
+                        <Box sx={{ width: '100%' }}>
+                            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                <Tabs value={this.state.tabvalue} onChange={this.handleTabChange} aria-label="basic tabs example">
+                                    <Tab label="Initial Dataset" {...a11yProps(0)} />
+                                    >
+                                </Tabs>
+                            </Box>
+
+                        </Box>
+                        <TabPanel value={this.state.tabvalue} index={0}>
+                        </TabPanel>
+
+
+                    </Grid>
+                </Grid>
+        )
+    }
+}
+
+export default GroupSleepSensitivityAnalysisPage;
--- src/components/base-components/WelcomePage.jsx
+++ src/components/base-components/WelcomePage.jsx
@@ -367,7 +367,7 @@ function WelcomePage() {
                                 size="medium"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%", borderLeft : "1px solid black", position: "absolute"}} />}
                                 // sx = {{display: dashboardMode === "dev" ? "block" : "none", backgroundColor: dashboardMode === "dev" ? "red" : "default"}}
-                                onClick={redirectToPage.bind(this,1, 1, 8, "group_sleep_analysis_sensitivity_add_subject_add_channels_final",
+                                onClick={redirectToPage.bind(this,1, 1, 8, "group_sleep_analysis",
                                         ["saved",
                                     "saved",
                                     "saved",
