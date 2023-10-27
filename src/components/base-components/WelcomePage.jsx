29fd2be543b3dffcf6f663e6e93a6ec12102d144
mikailo <m.kontoulis.cs@gmail.com>
2023-10-27 18:56:32 +0300

    -Changed location of function to new folder -Fixed navigation bug that vaused user to not login automatically to vm in some functions

--- src/components/base-components/WelcomePage.jsx
+++ src/components/base-components/WelcomePage.jsx
@@ -367,7 +367,7 @@ function WelcomePage() {
                                 size="medium"
                                 endIcon={<NavigateNextIcon sx={{right: "0%", top: "20%", borderLeft : "1px solid black", position: "absolute"}} />}
                                 // sx = {{display: dashboardMode === "dev" ? "block" : "none", backgroundColor: dashboardMode === "dev" ? "red" : "default"}}
-                                onClick={redirectToPage.bind(this,1, 1, 10, "group_sleep_analysis",
+                                onClick={redirectToPage.bind(this,1, 1, 8, "group_sleep_analysis",
                                         ["saved",
                                     "saved",
                                     "saved",
