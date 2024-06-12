ad104387f6a1816a32e054dee57570aa112464f3
gd180 <gdoukas@epu.ntua.gr>
2024-06-12 18:14:27 +0300

    new page DatasetConcat.jsx

--- src/components/base-components/App.jsx
+++ src/components/base-components/App.jsx
@@ -165,6 +165,7 @@ import XGBoostModelLoad from "../../pages/AI/XGBoostModelLoad";
 import XGBoostModelCreation from "../../pages/AI/XGBoostModelCreation";
 import AutoencoderModelCreation from "../../pages/AI/AutoencoderModelCreation";
 import AutoencoderModelLoad from "../../pages/AI/AutoencoderModelLoad";
+import DatasetConcat from "../../pages/hypothesis_testing/DatasetConcat";
 
 // Theme Colors Declaration
 let firstColor = '#59C7F3'
@@ -478,6 +479,9 @@ const App = () => {
                                        element={(<LayoutMain><AutoencoderModelCreation/></LayoutMain>)}/>
                                 <Route exact path="/AutoencoderModelLoad"
                                        element={(<LayoutMain><AutoencoderModelLoad/></LayoutMain>)}/>
+                                <Route exact path="/DatasetConcat"
+                                       element={(<LayoutMain><DatasetConcat/></LayoutMain>)}/>
+
                             </Routes>
                         </BrowserRouter>
                     </ThemeProvider>
